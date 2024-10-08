d2596790a710277f440522b46310d98cfac0dbc1
Evan Nadeau <evannadeau@users.noreply.github.com>
2024-10-08 06:45:28 -0700

    cleanup output

--- aws/infra/outputs.tf
+++ aws/infra/outputs.tf
@@ -1,5 +1,5 @@
 
-output "example_vpc_rtb_id" {
-  description = "ID of the main route table"
-  value       = aws_vpc.example.main_route_table_id
-}
+# output "example_vpc_rtb_id" {
+#   description = "ID of the main route table"
+#   value       = aws_vpc.example.main_route_table_id
+# }
